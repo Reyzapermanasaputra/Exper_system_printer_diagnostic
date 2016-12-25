dc616bb17e8d78456579df0193a9ecd6a352c4d0
Reyza Permana <reyza.permana@wgs.co.id>
2016-12-25 14:54:57 +0700

    expert system apps

---
+++ app/controllers/quizzes_controller.rb
@@ -0,0 +1,106 @@
+class QuizzesController < ApplicationController
+  def home
+    session[:number] = 3
+  end
+
+  def start
+    puts session[:number]
+    if session[:number] == 3
+      @indication = Indication.first(session[:number])
+    elsif session[:number] == 4
+      @indication = Indication.where(code: ['p04', 'p06'])
+    elsif session[:number] == 5
+      @indication = Indication.where(code: ['p05'])
+
+    elsif session[:number] == 7
+      @indication = Indication.where(code: ['p08', 'p10'])
+    elsif session[:number] == 8
+      @indication = Indication.where(code: ['p09'])
+    elsif session[:number] == 10
+      @indication = Indication.where(code: ['p07'])
+    elsif session[:number] == 12
+      @indication = Indication.where(code: ['p11'])
+    elsif session[:number] == 14
+      @indication = Indication.where(code: ['p12', 'p14'])
+    elsif session[:number] == 15
+      @indication = Indication.where(code: ['p13'])
+    elsif session[:number] == 17
+      @indication = Indication.where(code: ['p15'])
+    end
+  end
+
+  def answer
+    if params[:indication] == 'P01'
+      session[:number] = 4
+      redirect_to action: "start"
+    elsif params[:indication] == 'P04'
+      session[:number] = 5
+      redirect_to action: "start"
+    elsif params[:indication] == 'P05'
+      session[:number] = 6
+      redirect_to action: "end"
+
+
+    elsif params[:indication] == 'P02'
+      session[:number] = 7
+      redirect_to action: "start"
+    elsif params[:indication] == 'P08'
+      session[:number] = 8
+      redirect_to action: "start"
+    elsif params[:indication] == 'P09'
+      session[:number] = 9
+      redirect_to action: "end"
+
+    elsif params[:indication] == 'P06'
+      session[:number] = 10
+      redirect_to action: "start"
+    elsif params[:indication] == 'P07'
+      session[:number] = 11
+      redirect_to action: "end"
+
+    elsif params[:indication] == 'P10'
+      session[:number] = 12
+      redirect_to action: "start"
+    elsif params[:indication] == 'P11'
+      session[:number] = 13
+      redirect_to action: "end"
+
+    elsif params[:indication] == 'P03'
+      session[:number] = 14
+      redirect_to action: "start"
+    elsif params[:indication] == 'P12'
+      session[:number] = 15
+      redirect_to action: "start"
+    elsif params[:indication] == 'P13'
+      session[:number] = 16
+      redirect_to action: "end"
+
+    elsif params[:indication] == 'P14'
+      session[:number] = 17
+      redirect_to action: "start"
+    elsif params[:indication] == 'P15'
+      session[:number] = 18
+      redirect_to action: "end"
+    else
+      #code
+    end
+  end
+
+  def end
+    if session[:number] == 6
+      @solution = Solution.find_by_code('s01')
+    elsif session[:number] == 9
+      @solution = Solution.find_by_code('s03')
+    elsif session[:number] == 11
+      @solution = Solution.find_by_code('s02')
+    elsif session[:number] == 13
+      @solution = Solution.find_by_code('s04')
+    elsif session[:number] == 16
+      @solution = Solution.find_by_code('s05')
+    elsif session[:number] == 18
+      @solution = Solution.find_by_code('s06')
+    end
+    session[:number] = 1
+  end
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,52 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create{ name: 'Chicago' , { name: 'Copenhagen' }})
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Indication.first_or_create(
+  [
+    {code: 'P01', name: 'Printer Ink Jet'},
+    {code: 'P02', name: 'Printer Laser Jet'},
+    {code: 'P03', name: 'Printer Dot Matrix'},
+    {code: 'P04', name: 'Printer Bekerja tetapi tidak Ada Hasil Cetakan diKertas'},
+    {code: 'P05', name: 'Hasil cetakan Printer tidak sempurna / bergaris'},
+    {code: 'P06', name: 'Printer gagal menarik kertas'},
+    {code: 'P07', name: 'Kertas pada printer macet'},
+    {code: 'P08', name: 'Cetakan kabur / tidak rata'},
+    {code: 'P09', name: 'Cetakan berbayang'},
+    {code: 'P10', name: 'Titik - titik hitam pada halaman'},
+    {code: 'P11', name: 'Garis melintang pada halaman'},
+    {code: 'P12', name: 'Lampu error berkedap kedip'},
+    {code: 'P13', name: 'Lampu feed dan eror masih menyala tanpa berkedip tetapi tidak bisa mencetak'},
+    {code: 'P14', name: 'Printer Tidak bisa mencetak sepenuhnya'},
+    {code: 'P15', name: 'printer hanya mencetak satu kali'}
+  ]) 
+
+Solution.first_or_reate(
+  [
+    {code: 'S01', name: 'Lepas cartridge dengan hati-hati untuk mengecek apakah tinta sudah habis atau belum. Setelah itu lakukan pembersihan pada mat head nya dengan menggunakan cairan pembersih tinta'},
+    {code: 'S02', name: 'Membatasi tebal tumpukan kertas sesuai dengan kapasitas yang didukung oleh printer. Sebelum
+dipasang pada paper try, sebaiknya kertas
+dikibas-kibaskan terlebih dahulu agar kertas
+tidak saling menempel'},
+    {code: 'S03', name: 'Menggoyang toner printer agar lebih merata dan
+dapat dipakai kembali, namun tetap bersiap
+untuk mengisinya dengan yang baru.'},
+    {code: 'S04', name: 'Usaplah drum dengan kain halus untuk
+menghilangkan benda asing yang menempel atau
+dengan mengganti drum jika terdapat lubang
+kecil pada permukaan drum'},
+    {code: 'S05', name: 'Pastikan posisi kertas terpasang dengan baik,
+apabila sudah dilakukan tetapi lampu masih
+menyala kemungkinan sensor kertar printer
+rusak. Disarankan untuk mengganti sensor
+printer yan baru'},
+    {code: 'S06', name: 'Coba lakukan pengecekan di kabel cutternya,
+apakah ada yang putus atau tidak, dan cek juga
+apakah sensor gerak masih berfungsi dengan
+baik atau tidak dan lakukanlah pembersihan
+sensor dengan menggunakan cairan pembersih
+printer.'}
+  ]) 
